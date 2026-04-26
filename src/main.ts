86f1b671f022958d3f77c9dab41440d4c7240a91
Vladimir Privezenov <vladimir.privezenov@onlyoffice.com>
2026-04-27 01:54:23 +0300

    fix: add settings for global modules

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { winstonLogger } from './loggers/winston.logger';
+import { WinstonService } from './loggers/winston/winston.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const loggerInstance = winstonLogger;
+  const app = await NestFactory.create(AppModule, {
+    logger: new WinstonService(loggerInstance),
+  });
+  process.on('uncaughtException', async () => {
+    loggerInstance.error('uncaughtException in process');
+    if (app) {
+      await app.close();
+    }
+    process.exit(1);
+  });
+
+  process.on('unhandledRejection', async () => {
+    loggerInstance.error('unhandledRejection in process');
+    if (app) {
+      await app.close();
+    }
+    process.exit(1);
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
